589cedad97ce0ea217c6af9fbf54548fad84f252
kardibus <kardibus@gmail.com>
2022-10-02 12:36:40 +0500

    cors

--- src/main/kotlin/com/kardibus/beer/Controller.kt
+++ src/main/kotlin/com/kardibus/beer/Controller.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin
 class Controller(private var beerService: BeerService) {
 
     @GetMapping
